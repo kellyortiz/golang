cd643a4ac9a0b042e83019598683298de1c3a729
kellyortiz <31560921@mackenzista.com.br>
2021-03-28 20:57:09 -0300

    Colocação da função para mostrar os logs

--- Projeto/monitoramento.go
+++ Projeto/monitoramento.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -27,6 +28,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -116,4 +118,17 @@ func registraLog(site string, status bool) {
 	}
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 	arquivo.Close()
+}
+
+// restante do código omitido
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
 }
